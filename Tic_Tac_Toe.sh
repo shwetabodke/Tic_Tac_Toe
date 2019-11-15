9819df2bdc0bf02982b21a2b6df67b8e8098961a
shwetabodke <shwetabodke01@gmail.com>
2019-11-15 17:42:15 +0530

    added resetting or initializing of board

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,2 +1,21 @@
 #!/bin/bash -x
 echo "##############################  WELCOME TO TIC TAC TOE  ########################"
+
+# CONSTANTS
+declare BOARD_SIZE=9;
+
+# VARIABLES
+declare -A board;
+
+# INITIALIZING OR RESETTING BOARD
+function initializingBoard() {
+	for (( cell=1; $cell <= $BOARD_SIZE; cell++ ));
+		do
+			board[$cell]="_";
+		done
+
+	echo "All Keys" ${!board[@]}
+	echo "All Values" ${board[@]}
+}
+
+initializingBoard
